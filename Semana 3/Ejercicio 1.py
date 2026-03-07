32ca1cd20a8f8cc9b5e39cd429c9be6bea21b6ee
Jose2854 <jose.leguizamon@uniminuto.edu.co>
2026-03-06 20:58:02 -0500

    Mi primer programa

---
+++ Semana 3/Ejercicio 1.py
@@ -0,0 +1,18 @@
+#esto es un tipo de dato float
+a=20
+#esto es un tipo de dato entero
+b=3
+print("la suma es:", a+b)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+#residuo de la divisdion 
+print(a%b)
+print("el residuo es", a%b)
+
+#comparaciones 
+print(a<b)
+print(a>b)
+print(a==b)
